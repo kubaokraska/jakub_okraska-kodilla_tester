39b0f586f2c260351b43dc4ae95fe644e542db57
Jakub Okraska <kuba.okraska@tlen.pl>
2022-02-15 21:35:44 +0100

    Format kodu

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -4,20 +4,20 @@ import com.kodilla.stream.UsersRepository;
 
 public class ForumStats {
     public static void main(String[] args) {
-       double avgNumberOfPosts = UsersRepository.getUsersList()
+        double avgNumberOfPosts = UsersRepository.getUsersList()
                 .stream()
-                .filter(u -> u.getAge()>=40)
+                .filter(u -> u.getAge() >= 40)
                 .map(u -> u.getNumberOfPost())
-                .mapToInt(n ->n)
+                .mapToInt(n -> n)
                 .average()
                 .getAsDouble();
         System.out.println(avgNumberOfPosts);
 
         avgNumberOfPosts = UsersRepository.getUsersList()
                 .stream()
-                .filter(u -> u.getAge()<40)
+                .filter(u -> u.getAge() < 40)
                 .map(u -> u.getNumberOfPost())
-                .mapToInt(n ->n)
+                .mapToInt(n -> n)
                 .average()
                 .getAsDouble();
         System.out.println(avgNumberOfPosts);
