e3ce360c01ccc25ec6f496e1b2f2f015581d7e94
Jakub Okraska <kuba.okraska@tlen.pl>
2022-02-16 03:15:20 +0100

    Poprawka 7.1

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/UserValidatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/UserValidatorTestSuite.java
@@ -11,14 +11,14 @@ class UserValidatorTestSuite {
     // VALIDATE USERNAME METHOD TESTS
     @ParameterizedTest
     @ValueSource(strings = {"", "1", "PA", "pass@", "pass#", "pass!", "pass$"})
-    public void shouldReturnFalseIfStingIsNotWorkingWithRegex_validateUsernameMethod(String username) {
+    public void shouldReturnFalseIfStringIsNotWorkingWithRegex_validateUsernameMethod(String username) {
         boolean result = userValidator.validateUsername(username);
         assertFalse(result);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"123", "pas", "PAS", "pass.", "pass-", "_PA"})
-    public void shouldReturnTrueIfStingIsWorkingWithRegex_validateUsernameMethod(String username) {
+    public void shouldReturnTrueIfStringIsWorkingWithRegex_validateUsernameMethod(String username) {
         boolean result = userValidator.validateUsername(username);
         assertTrue(result);
     }
@@ -27,14 +27,14 @@ class UserValidatorTestSuite {
     @ParameterizedTest
     @ValueSource(strings = {"a", "1", "gmail.com", "@gmail.com", "a.@gmail.com", "user..name@gmail.com", "u@gmail..com",
             "u@gmail.com1", "u@gmail.com.pl1", "u @gmail.com", "u@gmail .com", "u@ gmail.com", "u@gmail. com"})
-    public void shouldReturnFalseIfStingIsNotWorkingWithRegex_validateEmailMethod(String email) {
+    public void shouldReturnFalseIfStringIsNotWorkingWithRegex_validateEmailMethod(String email) {
         boolean result = userValidator.validateEmail(email);
         assertFalse(result);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"user_.name_@gmai-.com.pl.de", "-user.-name@gmail-.com"})
-    public void shouldReturnTrueIfStingIsWorkingWithRegex_validateEmailMethod(String email) {
+    public void shouldReturnTrueIfStringIsWorkingWithRegex_validateEmailMethod(String email) {
         boolean result = userValidator.validateEmail(email);
         assertTrue(result);
     }
