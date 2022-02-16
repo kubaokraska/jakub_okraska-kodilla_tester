fea599424d9b8721495f7a42709ab3568603cfc0
Jakub Okraska <kuba.okraska@tlen.pl>
2022-02-16 03:12:21 +0100

    Zadanie 7.1 Ćwiczymy testy z parametram

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringValidatorTestSuite.java
@@ -0,0 +1,34 @@
+package com.kodilla.parametrized_tests;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringValidatorTestSuite {
+
+    private StringValidator validator = new StringValidator();
+
+    @Test
+    public void shouldReturnFalseIfStringIsNotEmpty() {
+        assertFalse(validator.isBlank("Text"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "   "})
+    public void shouldReturnTrueIfStringIsEmpty(String text) {
+        assertTrue(validator.isBlank(text));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void shouldReturnTrueIfStringIsNullOrEmpty(String text) {
+        System.out.println("|" + text + "|");
+        assertTrue(validator.isBlank(text));
+    }
+}
--- kodilla-stream/src/main/java/com/kodilla/exception/ThirdExample.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/ThirdExample.java
@@ -4,7 +4,7 @@ public class ThirdExample {
     public static void main(String[] args) {
         AirportRepository airportRepository = new AirportRepository();
         try {
-            boolean isViennaInUse = airportRepository.isAirportInUse("Vienna");
+            boolean isViennaInUse = airportRepository.isAirportInUse("Warsaw");
             System.out.println("Vienna status:" + isViennaInUse);
         } catch (AirportNotFoundException e) {
             System.out.println("Sorry, This ariponr cannot bla bla bla");
