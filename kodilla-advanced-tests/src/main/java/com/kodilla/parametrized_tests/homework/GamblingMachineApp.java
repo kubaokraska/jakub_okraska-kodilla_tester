f9bab9bf6568d5d562b5e47fe65b4730b1eaa448
Jakub Okraska <kuba.okraska@tlen.pl>
2022-02-18 01:54:23 +0100

    Zadanie 7.2 + Poprawka 7.1

---
+++ kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/GamblingMachineApp.java
@@ -0,0 +1,7 @@
+package com.kodilla.parametrized_tests.homework;
+
+public class GamblingMachineApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -0,0 +1,27 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GamblingMachineTestSuite {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/stringGamblingMachine.csv", numLinesToSkip = 1)
+    public void shouldReturnTrueWhenNumbersAreFromInterval(String input) throws InvalidNumbersException {
+        String[] numbersTable = input.split(" ");
+        Set<Integer> numbersSet = new HashSet<>();
+        GamblingMachine gamblingMachine = new GamblingMachine();
+
+        for (int i = 0; i < numbersTable.length; i++)
+            numbersSet.add(Integer.valueOf(numbersTable[i]));
+
+        int inputFinal = gamblingMachine.howManyWins(numbersSet);
+        System.out.println("WINS: " + inputFinal);
+        assertTrue(inputFinal > 0);
+    }
+}
