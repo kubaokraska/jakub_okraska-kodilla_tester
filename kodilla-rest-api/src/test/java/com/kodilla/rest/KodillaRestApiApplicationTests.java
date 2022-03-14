a8d589759755e4226dec5d9e786d478abd0227d0
Jakub Okraska <kuba.okraska@tlen.pl>
2022-03-14 01:25:07 +0100

    Zadanie 10.3

---
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/KodillaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
