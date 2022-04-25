cb4b5552c41552c99b7d8dcd45b9b7685075a6bd
Jakub Okraska <kuba.okraska@tlen.pl>
2022-04-25 19:05:26 +0200

    Zadanie 13.5 Poprawka

--- kodilla-selenium/src/main/java/com/kodilla/selenium/Allegro/AllegroTestCss.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/Allegro/AllegroTestCss.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class AllegroTestCss {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "home/andrzej/dev/codemy/selenium/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\KaToV-PC\\kodilla2\\Kodilla_tester\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.allegro.pl/");
         List<WebElement> allegroClassContainer = new ArrayList<>();
