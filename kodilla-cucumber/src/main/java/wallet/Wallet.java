9c652338c20d16fb280abd66e9cf14f4402b19c1
kubaokraska <97173821+kubaokraska@users.noreply.github.com>
2022-05-06 22:34:30 +0200

    Update Wallet.java

--- kodilla-cucumber/src/main/java/wallet/Wallet.java
+++ kodilla-cucumber/src/main/java/wallet/Wallet.java
@@ -3,11 +3,9 @@ package wallet;
 public class Wallet {
     private int balance = 0;
 
-    public Wallet() {
-    }
-
     public void deposit(int money) {
-        this.balance += money;}
+        this.balance += money;
+    }
 
     public void debit(int money) {
         this.balance -= money;
